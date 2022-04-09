c390060e8620bc2d6452023845765cd9f6b588de
Nagyandb <nagyandb@gmail.com>
2022-04-09 09:31:14 +0200

    Modulzáró Nagy András

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -7,17 +7,34 @@ package main;
 public class Theorem {
 
     /* Milyen primitív adattípusok léteznek a Java-ban? Sorold fel őket! - 1p
-        Ide jön a válaszod...
+        - Vannak egész szám típusok:
+        * byte
+        * short
+        * int
+        * long
+
+        - Vannak törtszám típusok:
+        * float
+        * double
+
+        - Továbbá vannak olyan primitív adattípusok, amleyek nem számtípusok:
+        * char ( ez egyetlen karaktert ad meg az Aascii ábécéből)
+        * boolean ( ez edig egy logikai változo, vagy igaz)true) vagy hamis (false) értéke lehet)
+
      */
 
 
     /* Mi a különbség a while és a do-while ciklusok között? - 2p
-        Ide jön a válaszod...
+        A while elöltesztelős ciklus másnéven, míg a do-while az ún. hátultesztelős, vagyis míg a while-ban a felttelt a
+        ciklus kulcsszava után írjuk ki, addíg a do-while esetében ezt a blokk után beírandó while kulcsszó után tesszük.
+        A while esetében tudjuk hogy hányszor kell iterálnunk, míg a do-while esetében nem.
+        A while-nál a feltétel vizsgálata a feltétel vizsgálata megeleőzi az utasításblokkot, míg a do-while esetében
+        annak vizsgálata az utasításblokk után történik meg.
      */
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
-        Ide jön a válaszod...
+
      */
 
 }
