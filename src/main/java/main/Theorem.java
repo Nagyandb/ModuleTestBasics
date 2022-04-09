1854ed4d59dd652c9d0fe2e81457998a8ef6ec98
Nagyandb <nagyandb@gmail.com>
2022-04-09 09:49:59 +0200

    Modulzáró Nagy András

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -34,6 +34,28 @@ public class Theorem {
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
+        A tömb nem más, mint azonos adattípusú változók, elemek halmaza, amelyben tárolhatjuk őket.
+        Előre meghatározott a hosszuk, amit nem kell a deklarálásnál megadni de a hosszukat nem tudjuk változtatni.
+        Az elemek mind kapnak egy sorszámot, ezek az indexek. Az indexelés 0-val kezdődik, és az utolsó elemet a tömb
+        hossza - 1 "képlettel" adhatunk meg. Például:
+        int[] numbers = new int [10]
+        első eleme: numbers[0]
+        utolsó eleme: numbers[numbers.length - 1]
+        Fontos  megemlíteni hogy abban az esetben ha itt például egy olyan elemre hivatkozunk, amely nem létezik akkor
+        hibaüzenetet kapunk: "ArrayIndexOutOfBoundsException"
+        Például ha a fenti tömbből a tömb 11-ik elemét akarjuk megadni az így nézne ki:
+        numbers[10] -- erre megkapnánk a fenti hibaüzenetet.
+
+        Léteznek többdimenziós tömbök is, vagyis egy tömbben lehet több masik is. Ha például veszünk egy kétdimenziós
+        tömböt, az sorokból és oszlopokból áll.
+        Ennek képlete például:
+        int numbersDimension[][] =  new int[3][8]
+
+
+        Itt például a tömb harmadik sorát ami a második indexen található, adtuk meg, és annak az utolsó, hetedik indexen levő
+        elemét
+        int elementOfArray = nubemrsDimension[2][7]
+
 
      */
 
